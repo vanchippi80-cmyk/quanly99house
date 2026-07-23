532a3af4e7e0e66f59ad4ef5b666ce18a28db489
Van <vanchippi80@gmail.com>
2026-07-23 11:35:37 +0700

    Revert "Add báo cáo tổng hợp"
    This reverts commit 36fd7829e9d4910be524a3cac360d6f3345ad712.

--- deploy.bat
+++ deploy.bat
@@ -1,24 +1,9 @@
 @echo off
-cd /d "C:\Users\anhtv\OneDrive\Documents\GitHub\quanly99house"
-
-:: Tim git tu GitHub Desktop
-set GIT=""
-for /f "delims=" %%i in ('where git 2^>nul') do set GIT="%%i" & goto :found
-for /f "delims=" %%i in ('dir /b /s "C:\Users\anhtv\AppData\Local\GitHubDesktop\app-*\resources\app\git\cmd\git.exe" 2^>nul') do set GIT="%%i" & goto :found
-for /f "delims=" %%i in ('dir /b /s "C:\Program Files\Git\cmd\git.exe" 2^>nul') do set GIT="%%i" & goto :found
-
-:found
-if %GIT%=="" (
-  echo KHONG TIM THAY GIT. Vui long cai Git hoac dung GitHub Desktop.
-  pause
-  exit /b 1
-)
-
-echo Tim thay git: %GIT%
+cd /d "%~dp0"
+echo === Deploy quanly99house ===
+git add index.html
+git commit -m "Update app"
+git push origin main
 echo.
-%GIT% add index.html
-%GIT% commit -m "Update app"
-%GIT% push origin main
-echo.
-echo === Hoan thanh! GitHub Pages cap nhat sau 1-2 phut ===
+echo === Hoan thanh! GitHub Pages se cap nhat trong 1-2 phut ===
 pause
